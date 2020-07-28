e089e693905b1af7bb7f28623494f9ec0cbcbe59
Kashyab19 <kashyabnarayanan3721@gmail.com>
2020-07-28 15:00:28 +0530

    Lecture 5

--- app.ts
+++ app.ts
@@ -1,42 +1,32 @@
 import { serve } from "https://deno.land/std/http/server.ts";
+import { acceptWebSocket, acceptable } from 'https://deno.land/std/ws/mod.ts';
+import { chatConnection } from './websocket/chatroom.ts';
 
-import {
-    acceptWebSocket,
-    isWebSocketCloseEvent,
-    isWebSocketPingEvent,
-    WebSocket,
-    acceptable
-  } from "https://deno.land/std/ws/mod.ts";
+// server setup
+const server = serve({ port: 3000 });
+console.log("http://localhost:3000/");
 
-import {chatConnection} from  "./websocket/chatroom.ts"
+for await (const req of server) {
+  
+  // serve index page
+  if (req.url === '/') {
+    req.respond({
+      status: 200,
+      body: await Deno.open('./public/index.html')
+    });
+  }
 
-//Setting up server:
-
-const server = serve({ port: 3010});
-console.log("http://localhost:3010/")
-
-for await (const req of server){
-    if(req.url === "/"){
-        req.respond({
-            status : 200,
-            body : await Deno.open("./public/index.html")
-        })
+  // accept the websocket connection
+  if (req.url === '/ws') {
+    if (acceptable(req)) {
+      acceptWebSocket({
+        conn: req.conn,
+        bufReader: req.r,
+        bufWriter: req.w,
+        headers: req.headers,
+      })
+      .then(chatConnection);
     }
-
-    //Accept WebSocket:
-    if(req.url === "/ws"){
-        if(acceptable(req)){
-            acceptWebSocket({
-                conn:req.conn,
-                bufReader:req.r,
-                bufWriter:req.w,
-                headers:req.headers,
-            })
-            .then(chatConnection)
-        }
-    }
+  }
+  
 }
-
-
-// //To run the app
-// denon run --allow-net --allow-read app.ts
--- websocket/chatroom.ts
+++ websocket/chatroom.ts
@@ -1,31 +1,42 @@
-import {WebSocket,isWebSocketCloseEvent} from "https://deno.land/std/ws/mod.ts";
-import { v4 } from "https://deno.land/std/uuid/mod.ts";
+import { WebSocket, isWebSocketCloseEvent } from 'https://deno.land/std/ws/mod.ts';
+import { v4 } from 'https://deno.land/std/uuid/mod.ts';
 
+let sockets = new Map<string, WebSocket>(); 
 
-let sockets = new Map<string,WebSocket>();
-
-const chatConnection = async(ws:WebSocket) =>{
-    console.log("Connected with Web Socket");
-    //Add new web socket conn to the map
-
-    const my_uid =  v4.generate();
-
-    sockets.set(my_uid, ws);
-
-    for await(const ev of ws){
-        console.log('Event of WS ==>'+ ev);
-
-
-        if(!isWebSocketCloseEvent(ev)){
-            sockets.delete(my_uid);
-        }
-        if(typeof(ev)=== 'string'){
-            let eventObject = JSON.parse(ev);
-            console.log('Event Object of WS ==>'+ eventObject);
-        }
-    }
-
-
+interface BroadcastObj {
+  name: string,
+  mssg: string,
 }
 
-export {chatConnection}
+// broadcast events to clients
+const broadcastEvent = (obj: BroadcastObj) => {
+  sockets.forEach((ws: WebSocket) => {
+    ws.send(JSON.stringify(obj));
+  });
+}
+
+const chatConnection = async (ws: WebSocket) => {
+  // add new ws connection to map
+  const uid = v4.generate();
+  sockets.set(uid, ws);
+
+  // listen for websocket events
+  for await (const ev of ws) {
+    console.log(ev);
+
+    // delete socket if connection closed
+    if (isWebSocketCloseEvent(ev)) {
+      sockets.delete(uid);
+    }
+    
+    // create ev object if ev is string
+    if (typeof ev === 'string') {
+      let evObj = JSON.parse(ev.toString());
+      broadcastEvent(evObj);
+    }
+
+  }
+
+};
+
+export { chatConnection };
